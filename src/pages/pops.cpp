aa94ca5d5858150a2dfd797e7c28e05513d55abb
quaoz <74419801+quaoz@users.noreply.github.com>
2024-12-10 02:31:41 +0000

    feat: working pops page

--- src/pages/pops.cpp
+++ src/pages/pops.cpp
@@ -4,18 +4,180 @@
 
 POPsPage::POPsPage(QWidget* parent) : QWidget(parent) {
   series = new QLineSeries();
+  redSeries = new QScatterSeries();
+  yellowSeries = new QScatterSeries();
+  greenSeries = new QScatterSeries();
+
+  redSeries->setBrush(Qt::red);
+  redSeries->setMarkerShape(QScatterSeries::MarkerShapeCircle);
+  redSeries->setMarkerSize(10.0);
+
+  yellowSeries->setBrush(Qt::yellow);
+  yellowSeries->setMarkerShape(QScatterSeries::MarkerShapeCircle);
+  yellowSeries->setMarkerSize(10.0);
+
+  greenSeries->setBrush(Qt::green);
+  greenSeries->setMarkerShape(QScatterSeries::MarkerShapeCircle);
+  greenSeries->setMarkerSize(10.0);
 
   QChart* chart = new QChart();
   chart->addSeries(series);
-  chart->setTitle("PCBs");
-  chart->createDefaultAxes();
+  chart->addSeries(redSeries);
+  chart->addSeries(yellowSeries);
+  chart->addSeries(greenSeries);
+  chart->setTitle("Persistent Organic Pollutants");
 
   chartView = new QChartView(chart);
   chartView->setRenderHint(QPainter::Antialiasing);
 
+  axisX = new QDateTimeAxis;
+  axisX->setFormat("MMM yyyy");
+  axisX->setTitleText("Date");
+  axisX->setTickCount(10);
+  chartView->chart()->addAxis(axisX, Qt::AlignBottom);
+  series->attachAxis(axisX);
+  redSeries->attachAxis(axisX);
+  yellowSeries->attachAxis(axisX);
+  greenSeries->attachAxis(axisX);
+
+  axisY = new QValueAxis;
+  axisY->setTitleText("Sample Quantity");
+  chartView->chart()->addAxis(axisY, Qt::AlignLeft);
+  series->attachAxis(axisY);
+  redSeries->attachAxis(axisY);
+  yellowSeries->attachAxis(axisY);
+  greenSeries->attachAxis(axisY);
+
+  locationComboBox = new QComboBox();
+  pbcsComboBox = new QComboBox();
+
+  pbcsComboBox->addItem("All POPs");
+  for (const auto& compound : pcbs) {
+    pbcsComboBox->addItem(QString::fromStdString(compound));
+  }
+
+  QHBoxLayout* options = new QHBoxLayout();
+  options->addWidget(pbcsComboBox);
+  options->addWidget(locationComboBox);
+
   QVBoxLayout* layout = new QVBoxLayout(this);
+  layout->addLayout(options);
   layout->addWidget(chartView);
   setLayout(layout);
+
+  connect(locationComboBox, &QComboBox::currentTextChanged, this,
+          &POPsPage::onComboBoxChanged);
+  connect(pbcsComboBox, &QComboBox::currentTextChanged, this,
+          &POPsPage::onComboBoxChanged);
+  connect(redSeries, &QScatterSeries::clicked, this,
+          &POPsPage::onDataPointClicked);
+  connect(yellowSeries, &QScatterSeries::clicked, this,
+          &POPsPage::onDataPointClicked);
+  connect(greenSeries, &QScatterSeries::clicked, this,
+          &POPsPage::onDataPointClicked);
 }
 
-void POPsPage::update(QuakeModel* model) { series->clear(); }
+void POPsPage::update(QuakeModel* model) {
+  this->model = model;
+  locationComboBox->clear();
+
+  locationComboBox->addItem("All Locations");
+  for (const auto& location : model->getLocations()) {
+    locationComboBox->addItem(QString::fromStdString(location));
+  }
+
+  filter();
+}
+
+void POPsPage::filter() {
+  series->clear();
+  redSeries->clear();
+  yellowSeries->clear();
+  greenSeries->clear();
+
+  QString selectedLocation = locationComboBox->currentText();
+  QString selectedCompound = pbcsComboBox->currentText();
+
+  chartView->chart()->setTitle(QString::fromStdString(
+    selectedCompound.toStdString() + " at " + selectedLocation.toStdString()));
+
+  double max = 0;
+  double redBoundary;
+  double greenBoundary;
+  QDateTime minDateTime;
+  QDateTime maxDateTime;
+
+  bool firstSample = true;
+
+  std::vector<Sample> samples = {};
+
+  if (selectedCompound.toStdString() == "All POPs") {
+    samples = model->getPollutantSamples(pcbs);
+  } else {
+    samples = model->getPollutantSamples(selectedCompound.toStdString());
+  }
+
+  for (const auto& sample : samples) {
+    QDateTime dateTime =
+      QDateTime::fromString(sample.getSampleDateTime().c_str(), Qt::ISODate);
+
+    if (selectedLocation.toStdString() == "All Locations" ||
+        sample.getSamplingPoint().getLabel() ==
+          selectedLocation.toStdString()) {
+      double result = sample.getResult();
+      if (result > max) { max = result; }
+
+      if (firstSample) {
+        minDateTime = dateTime;
+        maxDateTime = dateTime;
+        redBoundary = result * 1.1;
+        greenBoundary = result * 0.9;
+        firstSample = false;
+      } else if (dateTime < minDateTime) {
+        minDateTime = dateTime;
+      } else if (dateTime > maxDateTime) {
+        maxDateTime = dateTime;
+      }
+
+      series->append(dateTime.toMSecsSinceEpoch(), result);
+
+      if (result > redBoundary) {
+        redSeries->append(dateTime.toMSecsSinceEpoch(), result);
+      } else if (result < greenBoundary) {
+        greenSeries->append(dateTime.toMSecsSinceEpoch(), result);
+      } else {
+        yellowSeries->append(dateTime.toMSecsSinceEpoch(), result);
+      }
+    }
+  }
+
+  max *= 1.05;
+
+  axisX->setRange(minDateTime, maxDateTime);
+  axisY->setRange(0, max);
+}
+
+void POPsPage::onComboBoxChanged() { filter(); }
+
+void POPsPage::onDataPointClicked(const QPointF& point) {
+  QString complianceLevel;
+
+  if (redSeries->points().contains(point)) {
+    complianceLevel = "Not Compliant";
+  } else if (yellowSeries->points().contains(point)) {
+    complianceLevel = "Near Limit";
+  } else if (greenSeries->points().contains(point)) {
+    complianceLevel = "Compliant";
+  } else {
+    complianceLevel = "Unknown";
+  }
+
+  // Construct popup message
+  QString popupText = QString("Pollutant: %1\nValue: %2\nCompliance: %3")
+                        .arg("TODO: fix diff series")
+                        .arg(point.y())
+                        .arg(complianceLevel);
+
+  // Show the tooltip near the cursor
+  QToolTip::showText(QCursor::pos(), popupText, this);
+}
--- src/pages/pops.hpp
+++ src/pages/pops.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <QComboBox>
 #include <QValueAxis>
 #include <QWidget>
 #include <QtCharts/QChartView>
+#include <QtCharts/QDateTimeAxis>
 #include <QtCharts/QLineSeries>
+#include <QtCharts/QScatterSeries>
 
 #include "../data/model.hpp"
 
@@ -14,7 +17,24 @@ class POPsPage : public QWidget {
   POPsPage(QWidget* parent = nullptr);
   void update(QuakeModel* model);
 
+ private slots:
+  void onComboBoxChanged();
+
  private:
+  void filter();
+  void onDataPointClicked(const QPointF& point);
+
   QChartView* chartView;
   QLineSeries* series;
+  QScatterSeries* redSeries;
+  QScatterSeries* yellowSeries;
+  QScatterSeries* greenSeries;
+  QDateTimeAxis* axisX;
+  QValueAxis* axisY;
+  QComboBox* locationComboBox;
+  QComboBox* pbcsComboBox;
+  QuakeModel* model;
+  std::set<std::string> const pcbs = {
+    "PCB Con 028", "PCB Con 052", "PCB Con 101", "PCB Con 105", "PCB Con 118",
+    "PCB Con 138", "PCB Con 153", "PCB Con 156", "PCB Con 180", "PCBs"};
 };
